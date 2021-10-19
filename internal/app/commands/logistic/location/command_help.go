7416b0ed0bf40ad44cceeb0942566495bae10c63
Zaurbek Gasanov <zaurbekzet@gmail.com>
2021-10-20 00:06:52 +0300

    Add command arguments format to help message

--- internal/app/commands/logistic/location/command_help.go
+++ internal/app/commands/logistic/location/command_help.go
@@ -6,11 +6,11 @@ import (
 
 func (c *LogisticLocationCommander) Help(inputMsg *tgbotapi.Message) {
 	msgText := `/help__logistic__location — print list of commands
-/get__logistic__location — get location
+/get__logistic__location <id> — get location
 /list__logistic__location — get list of locations
-/delete__logistic__location — delete existing location
-/new__logistic__location — create new location
-/edit__logistic__location — edit location`
+/delete__logistic__location <id> — delete existing location
+/new__logistic__location {"title": "<title>", "latitude": <latitude>, "longitude": <longitude>} — create new location
+/edit__logistic__location {"id": <id>, "title": "<new_title>", "latitude": <new_latitude>, "longitude": <new_longitude>} — edit location`
 
 	c.sendMessage(inputMsg.Chat.ID, msgText, logPrefix+".Help")
 }
