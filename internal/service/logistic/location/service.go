54438730b3ecd1c776d5d2e571f52828517cb31b
Zaurbek Gasanov <zaurbekzet@gmail.com>
2021-10-19 16:35:17 +0300

    Add some initial data

--- internal/service/logistic/location/service.go
+++ internal/service/logistic/location/service.go
@@ -20,8 +20,8 @@ type DummyLocationService struct {
 
 func NewDummyLocationService() *DummyLocationService {
 	return &DummyLocationService{
-		locations: make([]logistic.Location, 0),
-		nextID:    1,
+		locations: initialLocations,
+		nextID:    uint64(len(initialLocations) + 1),
 	}
 }
 
@@ -91,4 +91,37 @@ type LocationNotFoundError struct {
 
 func (e *LocationNotFoundError) Error() string {
 	return fmt.Sprintf("location with ID %d not found", e.ID)
+}
+
+var initialLocations = []logistic.Location{
+	{
+		ID:        1,
+		Latitude:  -13.163060,
+		Longitude: -72.545560,
+		Title:     "Machu Picchu",
+	},
+	{
+		ID:        2,
+		Latitude:  20.683060,
+		Longitude: -88.568610,
+		Title:     "Chichen Itza",
+	},
+	{
+		ID:        3,
+		Latitude:  41.890169,
+		Longitude: 12.492269,
+		Title:     "Colosseum",
+	},
+	{
+		ID:        4,
+		Latitude:  27.174931,
+		Longitude: 78.042097,
+		Title:     "Taj Mahal",
+	},
+	{
+		ID:        5,
+		Latitude:  29.979167,
+		Longitude: 31.134167,
+		Title:     "Great Pyramid of Giza",
+	},
 }
