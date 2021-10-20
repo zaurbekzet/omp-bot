b7011e6d995e8e5e54196168e40f6f8fc13b537d
Zaurbek Gasanov <zaurbekzet@gmail.com>
2021-10-20 12:35:36 +0300

    Move initial locations to model file

--- internal/model/logistic/location.go
+++ internal/model/logistic/location.go
@@ -23,3 +23,36 @@ func (l Location) Validate() error {
 	}
 	return nil
 }
+
+var InitialLocations = []Location{
+	{
+		ID:        1,
+		Latitude:  -13.163060,
+		Longitude: -72.545560,
+		Title:     "Machu Picchu",
+	},
+	{
+		ID:        2,
+		Latitude:  20.683060,
+		Longitude: -88.568610,
+		Title:     "Chichen Itza",
+	},
+	{
+		ID:        3,
+		Latitude:  41.890169,
+		Longitude: 12.492269,
+		Title:     "Colosseum",
+	},
+	{
+		ID:        4,
+		Latitude:  27.174931,
+		Longitude: 78.042097,
+		Title:     "Taj Mahal",
+	},
+	{
+		ID:        5,
+		Latitude:  29.979167,
+		Longitude: 31.134167,
+		Title:     "Great Pyramid of Giza",
+	},
+}
--- internal/service/logistic/location/service.go
+++ internal/service/logistic/location/service.go
@@ -22,8 +22,8 @@ type DummyLocationService struct {
 
 func NewDummyLocationService() *DummyLocationService {
 	return &DummyLocationService{
-		locations: initialLocations,
-		nextID:    uint64(len(initialLocations) + 1),
+		locations: logistic.InitialLocations,
+		nextID:    uint64(len(logistic.InitialLocations) + 1),
 	}
 }
 
@@ -108,37 +108,4 @@ type LocationNotFoundError struct {
 
 func (e *LocationNotFoundError) Error() string {
 	return fmt.Sprintf("location with ID %d not found", e.ID)
-}
-
-var initialLocations = []logistic.Location{
-	{
-		ID:        1,
-		Latitude:  -13.163060,
-		Longitude: -72.545560,
-		Title:     "Machu Picchu",
-	},
-	{
-		ID:        2,
-		Latitude:  20.683060,
-		Longitude: -88.568610,
-		Title:     "Chichen Itza",
-	},
-	{
-		ID:        3,
-		Latitude:  41.890169,
-		Longitude: 12.492269,
-		Title:     "Colosseum",
-	},
-	{
-		ID:        4,
-		Latitude:  27.174931,
-		Longitude: 78.042097,
-		Title:     "Taj Mahal",
-	},
-	{
-		ID:        5,
-		Latitude:  29.979167,
-		Longitude: 31.134167,
-		Title:     "Great Pyramid of Giza",
-	},
 }
